cddeefb289e22807b1cd4f98d854b4d3ca3b72b4
nhasty <nyall.hasty@gmail.com>
2022-09-08 16:10:14 -0700

    functional-shared stack and queue complete

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  // Use an object with numeric keys to store values
+  someInstance.storage = {};
+  someInstance.enqueue = queueMethods.enqueue;
+  someInstance.dequeue = queueMethods.dequeue;
+  someInstance.size = queueMethods.size;
+  return someInstance;
 };
 
 var queueMethods = {};
-
+queueMethods.enqueue = function (value) {
+  var currentQ = this.storage;
+  var qPosition = 1;
+  if (Object.keys(currentQ).length) {
+    qPosition = Math.max.apply(Math, Object.keys(currentQ)) + 1;
+  }
+  currentQ[qPosition] = value;
+};
+queueMethods.dequeue = function () {
+  var currentQ = this.storage;
+  var minKey = 1;
+  minKey = Math.min.apply(Math, Object.keys(currentQ));
+  var leaveQ = this.storage[minKey];
+  delete this.storage[minKey];
+  return leaveQ;
+};
+queueMethods.size = function () {
+  return Object.keys(this.storage).length;
+}
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,49 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // define instance object variable
+  var someInstance = {};
+  // add storage object to instance
+  someInstance.storage = {};
+  // add push to instance
+  someInstance.push = stackMethods.push;
+  // add pop to instance
+  someInstance.pop = stackMethods.pop;
+  // add size to insstance
+  someInstance.size = stackMethods.size;
+  // return instance
+  return someInstance;
 };
 
 var stackMethods = {};
-
+// define push
+stackMethods.push = function(value) {
+  // define max key variable equal to 1
+  currentStack = this.storage;
+  var maxKey = 1;
+  // if storage is not empty
+  if (Object.keys(currentStack).length) {
+    // adjust max key
+    maxKey = Math.max.apply(Math, Object.keys(currentStack)) + 1;
+  }
+  // add value to storage at maxkey
+  currentStack[maxKey] = value;
+};
+// define pop
+stackMethods.pop = function() {
+  // define max key variable
+  currentStack = this.storage;
+  var maxKey = Math.max.apply(Math, Object.keys(currentStack));
+  // capture value at max key
+  var leaveStack = currentStack[maxKey];
+  // delete max key property from storage
+  delete currentStack[maxKey];
+  // return captured value
+  return leaveStack;
+};
+// define size
+stackMethods.size = function() {
+  //return length of storage keys array
+  return Object.keys(this.storage).length;
+}
 
