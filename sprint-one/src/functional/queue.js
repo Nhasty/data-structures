11c859d6af645015b166a9104c6c37f5d3ca214f
nhasty <nyall.hasty@gmail.com>
2022-09-08 13:25:00 -0700

    functional format q and stack complete

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,29 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // store value last in q
+    // initialize qPosition variable at front of q
+    var qPosition = 1;
+    // if q is not empty adjust qPosition to back of q
+    if (Object.keys(storage).length ) {
+      qPosition = Math.max.apply(Math, Object.keys(storage)) + 1;
+    }
+    // store value at q position
+    storage[qPosition] = value;
   };
 
   someInstance.dequeue = function() {
+    // remove first person i q from q
+    //debugger
+    var minKey = Math.min.apply(Math, Object.keys(storage));
+    var leaveQ = storage[minKey];
+    delete storage[minKey];
+    return leaveQ;
   };
 
   someInstance.size = function() {
+    // return q size (length of storgage object keys)
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,33 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //add to back of line
+    // define max key
+    var maxKey = 1;
+    // if storage is not empty
+    if (Object.keys(storage).length) {
+      // adjust max key
+      var maxKey = Math.max.apply(Math, Object.keys(storage)) + 1;
+    }
+    // add value
+    storage[maxKey] = value;
   };
 
   someInstance.pop = function() {
+    // remove last item
+    //find max key
+    var maxKey = Math.max.apply(Math, Object.keys(storage));
+    // collect value at last key
+    var leaveStack = storage[maxKey];
+    // remove value at last key
+    delete storage[maxKey];
+    // return collected value
+    return leaveStack;
   };
 
   someInstance.size = function() {
+    // return length of object keys array
+    return Object.keys(storage).length;
   };
 
   return someInstance;
