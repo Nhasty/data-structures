2968dc9ee7f387f13bdcaad6c26f08867fafe795
nhasty <nyall.hasty@gmail.com>
2022-09-08 19:01:00 -0700

    pseudoclassical stack and queue complete

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+};
+
+Queue.prototype.enqueue = function (value) {
+
+  var currentQ = this.storage;
+  var qPosition = 1;
+  if (this.size()) {
+    qPosition = Math.max.apply(Math, Object.keys(currentQ)) + 1;
+  }
+  currentQ[qPosition] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  var currentQ = this.storage;
+  var minKey = Math.min.apply(Math, Object.keys(currentQ));
+  var leaveQ = currentQ[minKey];
+  delete currentQ[minKey];
+  return leaveQ;
+};
+
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  var currentStack = this.storage;
+  var maxKey = 1;
+  if (this.size()) {
+    maxKey = Math.max.apply(Math, Object.keys(currentStack)) + 1;
+  }
+  currentStack[maxKey] = value;
+};
 
+Stack.prototype.pop = function() {
+  var currentStack = this.storage;
+  var maxKey = Math.max.apply(Math, Object.keys(currentStack));
+  var leaveStack = currentStack[maxKey];
+  delete currentStack[maxKey];
+  return leaveStack;
+};
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
