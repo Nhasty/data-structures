d5d7df6e83da7595b6337b7684e078954b0fae4d
nhasty <nyall.hasty@gmail.com>
2022-09-08 19:46:44 -0700

    es6 queue and stack complete

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  enqueue(value) {
+    var currentQ = this.storage;
+    var qPosition = 1;
+    if (this.size()) {
+      qPosition = Math.max.apply(Math, Object.keys(currentQ)) + 1;
+    }
+    currentQ[qPosition] = value;
+  }
+
+  dequeue() {
+    var currentQ = this.storage;
+    var minKey = Math.min.apply(Math, Object.keys(currentQ));
+    var leaveQ = currentQ[minKey];
+    delete currentQ[minKey];
+    return leaveQ;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+    var currentStack = this.storage;
+    var maxKey = 1;
+    if (this.size()) {
+      maxKey = Math.max.apply(Math, Object.keys(currentStack)) + 1;
+    }
+    currentStack[maxKey] = value;
+  }
+
+  pop() {
+    var currentStack = this.storage;
+    var maxKey = Math.max.apply(Math, Object.keys(currentStack));
+    var leaveStack = currentStack[maxKey];
+    delete currentStack[maxKey];
+    return leaveStack;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
