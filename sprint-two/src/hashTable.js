b9142b53f0e5c7a13cb40e46ea816cd85b7d07c0
nhasty <nyall.hasty@gmail.com>
2022-09-10 13:07:31 -0700

    completed set, hashtable pseudocode

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,15 +6,42 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+  //convert key to index
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // if index is empty
+  if (!this._storage[index]) {
+    // create empty array at index (limited array)
+           // LOOK INTO HOW EACH IS USED IN HASH TABLE HELPERS TO GET CLUE TO WHATS SUPPOSED TO BE STORED AT EACH INDEX
+    this._storage[index] = [];
+  }
+  // push [k, v] to list at index
+  this._storage[index].push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var atIndex = this._storage[index];
+  //if array at index
+  if (atIndex) {
+    // iterate through array
+    for (var i = 0; i < atIndex.length; i++) {
+      // if tuple[0] contains k
+      if (atIndex[i][0] === k) {
+        // return tuple[1]
+        return atIndex[i];
+      }
+    }
+  }
+  // return undefined
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // if array at index
+    // iterate through array
+     // if tuple[0] is key
+       //remove it from list
+  // if array at index is empty at end remove array
 };
 
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,24 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // object
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // assign storage at key = item to empty object
+  this._storage[item] = {};
 };
 
 setPrototype.contains = function(item) {
+  //collect keys and see if they include item
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item) {
+  //delete value at set storage key = item
+  delete this._storage[item];
 };
 
 /*
